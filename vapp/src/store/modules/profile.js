f957134966e97775f170c701cb29db03c318931e
Remote Gildor <73232606+remote-gildor@users.noreply.github.com>
2020-10-23 14:53:45 +0200

    using call instead of cacheCall

--- vapp/src/store/modules/profile.js
+++ vapp/src/store/modules/profile.js
@@ -10,41 +10,14 @@ const getters = {
 
 const actions = {
   async fetchTestTokenBalance({ commit, rootState }) {
-    /*
     let drizzleInstance = rootState.drizzle.drizzleInstance;
     let activeAccount = rootState.accounts.activeAccount;
 
-    window.console.log(drizzleInstance);
-
-    let drizzleState = drizzleInstance.store.getState();
-    window.console.log(drizzleState);
-
-    const dataKey =  await drizzleInstance.contracts.TestToken.methods.balanceOf.cacheCall(activeAccount);
-    window.console.log(dataKey);
-
-    window.console.log(drizzleState.contracts.TestToken.balanceOf);
-
-    const balanceSmall = drizzleState.contracts.TestToken.balanceOf[dataKey]["value"];
-    window.console.log(balanceSmall);
+    // get token balance for the active user
+    const smallUnitBalance = await drizzleInstance.contracts.TestToken.methods.balanceOf(activeAccount).call()
 
     // remove the 18 decimals and commit as balance
-    commit("setTestTokenBalance", drizzleInstance.web3.utils.fromWei(balanceSmall, "ether"));
-    */
-   
-    let drizzleInstance = rootState.drizzle.drizzleInstance;
-    let web3 = drizzleInstance.web3;
-    let activeAccount = rootState.accounts.activeAccount;
-
-    let testTokenContract = new web3.eth.Contract(
-      drizzleInstance.contracts.TestToken.abi,
-      drizzleInstance.contracts.TestToken.address
-    );
-
-    // call balanceOf function
-    let smallUnitBalance = await testTokenContract.methods.balanceOf(activeAccount).call();
-
-    // remove the 18 decimals and commit as balance
-    commit("setTestTokenBalance", web3.utils.fromWei(smallUnitBalance, "ether"));
+    commit("setTestTokenBalance", drizzleInstance.web3.utils.fromWei(smallUnitBalance, "ether"));
   }
 };
 
